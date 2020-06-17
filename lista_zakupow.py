74c05eef68dee8e616eb304a1015c87e86cd7702
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-06-17 20:23:39 +0200

    Test branches

---
+++ lista_zakupow.py
@@ -0,0 +1,13 @@
+lista_zakupow = {"piekarnia":["chleb","bulki","paczek"],\
+                "warzywniak":["marchew","seler","rukola"]}
+sum_of_products = 0
+
+for item in lista_zakupow:
+    produkty = ", ".join(lista_zakupow.get(item)).title()
+    print(f"Idę do {str.capitalize(item)}, żeby kupić{produkty}")
+    sum_of_products +=len(lista_zakupow.get(item))
+
+print(f"W sklepie kupuję {sum_of_products} produktów")     
+print("Dodatkowy test")   
+print("Jeszcze jeden dodatkowy test")
+
